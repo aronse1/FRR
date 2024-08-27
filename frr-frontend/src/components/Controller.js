a9d64fc151cf9738abbb9551a64763b79bcf6f85
philipp-engelhardt <123982724+philipp-engelhardt@users.noreply.github.com>
2024-08-27 17:22:21 +0200

    added WebSocket.OPEN check

--- frr-frontend/src/components/Controller.js
+++ frr-frontend/src/components/Controller.js
@@ -70,6 +70,7 @@ const Controller = () => {
 
     wsControlRef.current.onerror = () => {
       setIsControlActive(false);
+      
     };
 
     return () => {
@@ -81,19 +82,19 @@ const Controller = () => {
 
   useEffect(() => {
     const handleKeyDown = (event) => {
-      if (isControlActive && wsControlRef.current) {
+      if (isControlActive && wsControlRef.current && wsControlRef.current.readyState === WebSocket.OPEN) {
         wsControlRef.current.send(JSON.stringify({ type: 'keydown', key: event.key }));
         setPressedKey(event.key);
       }
     };
-
+  
     const handleKeyUp = (event) => {
-      if (isControlActive && wsControlRef.current) {
+      if (isControlActive && wsControlRef.current && wsControlRef.current.readyState === WebSocket.OPEN) {
         wsControlRef.current.send(JSON.stringify({ type: 'keyup', key: event.key }));
         setPressedKey(null);
       }
     };
-
+  
     if (isControlActive) {
       window.addEventListener('keydown', handleKeyDown);
       window.addEventListener('keyup', handleKeyUp);
@@ -102,7 +103,7 @@ const Controller = () => {
       window.removeEventListener('keyup', handleKeyUp);
       setPressedKey(null);
     }
-
+  
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
       window.removeEventListener('keyup', handleKeyUp);
