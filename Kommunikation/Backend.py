92e93cec0a6a3b5ce6cbdf4f7c0cc1404d420b02
Aron Seidl <aron.seidl@zeiss.com>
2024-09-04 12:48:51 +0200

    Information websocket route

--- Kommunikation/Backend.py
+++ Kommunikation/Backend.py
@@ -5,6 +5,7 @@ import websockets
 
 connected_image_clients = set()
 connected_movement_clients = set()
+conected_information_clients = set()
 
 last_movement_message = "idle"
 
@@ -21,6 +22,8 @@ async def image_handler(websocket, path):
                         connected_image_clients.remove(client)
             if tasks:
                 await asyncio.wait(tasks)
+    except websockets.ConnectionClosedError:
+        print(f"Client {websocket} disconnected had an error")
     except websockets.ConnectionClosed:
         print(f"Client {websocket} disconnected")
     finally:
@@ -48,16 +51,40 @@ async def movement_handler(websocket, path):
 
                 # Aktualisiere die letzte Nachricht
                 last_movement_message = message
+    except websockets.ConnectionClosedError:
+        print(f"Client {websocket} disconnected had an error")
     except websockets.ConnectionClosed:
         print(f"Client {websocket} disconnected")
     finally:
         connected_movement_clients.remove(websocket)
 
+
+async def information_handler(websocket, path):
+    conected_information_clients.add(websocket)
+    try:
+        async for message in websocket:
+            tasks = []
+            for client in conected_information_clients:
+                if client != websocket:
+                    try:
+                        tasks.append(asyncio.create_task(client.send(message)))
+                    except websockets.ConnectionClosed:
+                        connected_image_clients.remove(client)
+            if tasks:
+                await asyncio.wait(tasks)
+    except websockets.ConnectionClosedError:
+        print(f"Client {websocket} disconnected had an error")
+    except websockets.ConnectionClosed:
+        print(f"Client {websocket} disconnected")
+    finally:
+        conected_information_clients.remove(websocket)
+
+
 async def main():
     image_server = await websockets.serve(image_handler, "0.0.0.0", 5000)
     movement_server = await websockets.serve(movement_handler, "0.0.0.0", 5001)
-
-    await asyncio.gather(image_server.wait_closed(), movement_server.wait_closed())
+    information_server = await websockets.serve(information_handler, "0.0.0.0", 5002)
+    await asyncio.gather(image_server.wait_closed(), movement_server.wait_closed(), information_server.wait_closed())
 
 
 asyncio.get_event_loop().run_until_complete(main())
